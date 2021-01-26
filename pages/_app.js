03306a652b766e1d4a5bfcfc9bf22341c355817f
augustorl <gutin22@gmail.com>
2021-01-26 16:36:28 -0300

    adding favicon and og:image

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,12 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <link rel="icon" href="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/harbinger-l10-website/pt-br/production/pt-br/static/favicon-0cf29ce019f7cd1e7b24f85ab6ff97da.ico"/>
+        <meta property="og:description" content="Um quiz de League of Legends construído com React e NextJS"></meta>
+        <meta property="og:image" content="https://steamuserimages-a.akamaihd.net/ugc/860612122499646923/0F1F75ED219A20544E528A790D23016B1A34E37B/"></meta>
+        <meta property="og:image:type" content="image/jpeg"></meta>
+        <meta property="og:image:width" content="800"></meta>
+        <meta property="og:image:height" content="600"></meta>
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
