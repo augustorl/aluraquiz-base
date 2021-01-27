a53c4d0e7ee74efea8d30d1dd30d928b67e55434
augustorl <gutin22@gmail.com>
2021-01-27 19:51:41 -0300

    creating Button Js

---
+++ src/components/Button/index.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  border: 0;
+  min-width: 55px;
+  height: 32px;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 1;
+  text-transform: uppercase;
+  outline: 0;
+  transition: .3s;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    opacity: .7;
+  }
+  &:disabled {
+    background-color: #a9a9a9;
+    cursor: not-allowed;
+  }
+`;
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['submit', 'type', 'button']).isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Button;
