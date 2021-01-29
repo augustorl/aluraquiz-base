700c9c7efe803908ee54608e49495233019d708c
augustorl <gutin22@gmail.com>
2021-01-29 00:21:40 -0300

    fix input css

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,13 +4,15 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const InputBase = styled.input`
-    width: 100%;
-    border: none;
-    padding: 8px;
-    font-size: 14px;
-    color: ${({ theme }) => theme.colors.mainBG};
-    background-color: ${({ theme }) => theme.colors.mainBG};
-    outline: 0;
+  width: 100%;
+  padding: 15px;
+  font-size: 14px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  outline: 0;
+  margin-bottom: 15px;
 `;
 
 function Input({ onChange, placeholder, ...props }) {
@@ -28,7 +30,7 @@ Input.defaultProps = {
 Input.propTypes = {
   onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string.isRequired,
-  name: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
   value: PropTypes.string,
 };
 export default Input;
