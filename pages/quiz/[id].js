627c1b2b9304aed6f53b0c9df52b04e1b222da53
augustorl <gutin22@gmail.com>
2021-01-29 00:20:47 -0300

    creating next class challenge

---
+++ pages/quiz/[id].js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function QuizDaGalera() {
+  return (
+    <div>
+      Desafio da próxima aula
+    </div>
+  );
+}
+
+export default QuizDaGalera;
