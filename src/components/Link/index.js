f540ed96aea76d0adf85e1889b028b2c2c1877e0
augustorl <gutin22@gmail.com>
2021-02-05 13:53:26 -0300

    Add: Link component

---
+++ src/components/Link/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import NextLink from 'next/link';
+
+function Link({ children, href, ...props }) {
+  return (
+    <NextLink href={href} passHref>
+      <a {...props}>
+        {children}
+      </a>
+    </NextLink>
+
+  );
+}
+
+export default Link;
