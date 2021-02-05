7e364e762846680d4d6da28ade790fe6833260bc
augustorl <gutin22@gmail.com>
2021-02-05 13:53:43 -0300

    Add: QuizContainer Component

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -4,7 +4,7 @@ const QuizContainer = styled.div`
   width: 100%;
   max-width: 340px;
   padding-top: 15px;
-  margin: auto 6%;
+  margin: auto 8%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
